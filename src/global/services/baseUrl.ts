0d3fa33325a0eb13d093a63b2698b1b756dffcbf
Kevin Vettorazzi De Gasperi <fly.sub2@gmail.com>
2023-04-27 13:59:31 -0300

    Alteração de baseURL para teste

--- src/global/services/baseUrl.ts
+++ src/global/services/baseUrl.ts
@@ -1,2 +1,2 @@
 
-export const baseURL = 'https://25e2-2804-14c-7d91-93de-b014-de66-2383-a194.sa.ngrok.io'
+export const baseURL = 'https://5056-2804-14c-7d91-93de-9560-e4e7-732d-eb09.ngrok-free.app'
